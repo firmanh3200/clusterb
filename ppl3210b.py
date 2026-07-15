1f41f390a7c64a58d18c088074607a0014a048e3
fhy3273 <135565052+firmanh3200@users.noreply.github.com>
2026-07-15 22:32:19 +0700

    Refactor SE2026 Dashboard data processing
    Refactor code to improve data processing and visualization for the SE2026 Dashboard.

--- ppl3210b.py
+++ ppl3210b.py
@@ -49,6 +49,10 @@ df_sls = df_sls.sort_values(by=['nama_kab', 'nama_kec', 'nama_kel', 'sls'])
 df_sls2 = df_sls[['nama_kec', 'nama_kel', 'sls', 'nama_lengkap', 'email', 'no_telp', 'target', 'open_val', 'submit', 'pendataan', 'percentage', 'percentage_pendataan', 'progress_difference', 'rerata_per_24_jam', 'selisih_jam']]
 df_sls2 = df_sls2.sort_values(by=['nama_kec', 'nama_kel', 'sls'])
 
+df_sls2['sls2'] = df_sls2['sls'].str.replace(r'\s*\([^)]*\)\s*$', '', regex=True)
+
+sls_didata = df_sls2[df_sls2['pendataan'] != 0].copy()
+sls_belum = df_sls2[df_sls2['pendataan'] == 0].copy()
 
 ## USAHA
 url_usaha = "https://simpul-jabar.32net.id/api/usaha-data-rekap?kdkab=3210%20-%20KAB.%20MAJALENGKA&kdkec=&kdkel=&level=sls"
@@ -92,9 +96,36 @@ df_kk = df_qk[['kec', 'desa', 'sls', 'target_keluarga', 'k_baru', 'k_bersedia', 
        'k_tidak_ditemui', 'k_tidak_ditemukan', 'k_tidak_eligible']]
 df_kk = df_kk.sort_values(by=['kec', 'desa', 'sls'])
 
+# Samakan tipe data menjadi string terlebih dahulu
+df_bku['kec'] = df_bku['kec'].astype(str)
+df_bku['desa'] = df_bku['desa'].astype(str)
+df_bku['sls'] = df_bku['sls'].astype(str)
+
+df_sls2['nama_kec'] = df_sls2['nama_kec'].astype(str).str.strip()
+df_sls2['nama_kel'] = df_sls2['nama_kel'].astype(str).str.strip()
+df_sls2['sls2'] = df_sls2['sls2'].astype(str)
+
+# Langkah 1: Merge berdasarkan 3 kolom kunci
+kolom_yang_diperlukan = ['nama_kec', 'nama_kel', 'sls2', 'nama_lengkap', 'email', 'no_telp']
+df_sls2_subset = df_sls2[kolom_yang_diperlukan]
+
+# 2. Lakukan merge dengan menyesuaikan nama kolom kuncinya
+df_bku = df_bku.merge(
+    df_sls2_subset,
+    how='left', 
+    left_on=['kec', 'desa', 'sls'],           # Nama kolom di df_bku
+    right_on=['nama_kec', 'nama_kel', 'sls2'] # Nama kolom di df_sls2
+)
+
+# 3. Buang kolom kunci bawaan df_sls2 yang sudah tidak diperlukan lagi
+# Karena nama kolomnya berbeda, merge akan menghasilkan 6 kolom kunci.
+# Kita hapus 3 kolom milik df_sls2 ('nama_kec', 'nama_kel', 'sls2') karena sudah diwakili oleh 'kec', 'desa', 'sls'
+df_bku = df_bku.drop(columns=['nama_kec', 'nama_kel', 'sls2'])
+
+usaha_ppl = df_bku.groupby(by=['kec', 'nama_lengkap', 'email'])[['target_usaha', 'bku_baru', 'bku_baru_non', 'bku_baru_pertanian', 'bku_ditemukan', 'bku_ganda', 'bku_tdk_ditemukan', 'bku_temu_non', 'bku_temu_pertanian', 'bku_tutup', 'uk_baru', 'uk_baru_non', 'uk_baru_pertanian', 'uk_ditemukan', 'uk_ganda', 'uk_tdk_ditemukan', 'uk_temu_non', 'uk_temu_pertanian', 'uk_tutup']].sum().reset_index()
 
 with st.container(border=True):
-       st.header("Progress Sensus Ekonomi 2026 Kabupaten Majalengka")
+       st.header("Progress SE2026 Kabupaten Majalengka")
        st.caption("Sumber: simpul-jabar.32net.id")
 
 tab_ppl, tab_sls, tab_usaha, tab_qc = st.tabs(['PPL', 'SLS', 'PENDATAAN USAHA', 'PENDATAAN KELUARGA'])
@@ -105,40 +136,79 @@ with tab_ppl:
 
 with tab_sls:
     st.subheader("Progress SLS")
-    st.dataframe(df_sls2, width='stretch', hide_index=True)
-
+    st.dataframe(sls_didata, width='stretch', hide_index=True)
+    st.divider()
+    st.subheader("SLS Belum Didata")
+    st.dataframe(sls_belum, width='stretch', hide_index=True)
+    
 with tab_usaha:
     st.subheader("Progress Pendataan Usaha")
     df_bku['kab'] = 'KAB. MAJALENGKA'
     df_bku_kab = df_bku.groupby(by=['kab'])[['target_usaha', 'bku_baru', 'bku_baru_non', 'bku_baru_pertanian', 'bku_ditemukan', 'bku_ganda', 'bku_tdk_ditemukan', 'bku_temu_non', 'bku_temu_pertanian', 'bku_tutup', 'uk_baru', 'uk_baru_non', 'uk_baru_pertanian', 'uk_ditemukan', 'uk_ganda', 'uk_tdk_ditemukan', 'uk_temu_non', 'uk_temu_pertanian', 'uk_tutup']].sum().reset_index()
     
-    grafik_bku_kab = px.bar(df_bku_kab, x='kab', y=['target_usaha', 'bku_ditemukan', 'bku_tdk_ditemukan', 'bku_ganda', 'bku_tutup', 'bku_baru'], barmode='group', title="Capaian Pendataan BKU", labels={'value':'Jumlah', 'variable':'Status'})
+    grafik_bku_kab = px.bar(df_bku_kab, x='kab', y=['bku_ditemukan', 'bku_tdk_ditemukan', 'bku_ganda', 'bku_tutup', 'bku_baru'], barmode='group', title="Capaian Pendataan BKU", labels={'value':'Jumlah', 'variable':'Status'})
+    grafik_bku_kab.update_yaxes(
+        range=[0, 210000],
+        tickformat=",.0f" # Menambahkan koma sebagai pemisah ribuan
+    )
     
-    with st.container(border=True):    
-        st.plotly_chart(grafik_bku_kab, width="content")
+    grafik_uk_kab = px.bar(df_bku_kab, x='kab', y=['uk_ditemukan', 'uk_tdk_ditemukan', 'uk_ganda', 'uk_tutup', 'uk_baru'], barmode='group', title="Capaian Pendataan Usaha Keluarga", labels={'value':'Jumlah', 'variable':'Status'})
+    grafik_uk_kab.update_yaxes(
+        range=[0, 210000],
+        tickformat=",.0f" # Menambahkan koma sebagai pemisah ribuan
+    )
+
+    grafik_target_usaha = px.bar(df_bku_kab, x='kab', y='target_usaha', title="Target Usaha")
+    grafik_target_usaha.update_yaxes(
+        range=[0, 210000],
+        tickformat=",.0f" # Menambahkan koma sebagai pemisah ribuan
+    )
+
+    kol1c, kol2c, kol3c = st.columns([3, 2, 3])
     
-    with st.expander("PER SLS"):
-        st.dataframe(df_bku, width='stretch', hide_index=True)
-    with st.expander("REKAP DESA"):
-        df_bku_desa = df_bku.groupby(by=['kec', 'desa'])[['target_usaha', 'bku_baru', 'bku_baru_non', 'bku_baru_pertanian', 'bku_ditemukan', 'bku_ganda', 'bku_tdk_ditemukan', 'bku_temu_non', 'bku_temu_pertanian', 'bku_tutup', 'uk_baru', 'uk_baru_non', 'uk_baru_pertanian', 'uk_ditemukan', 'uk_ganda', 'uk_tdk_ditemukan', 'uk_temu_non', 'uk_temu_pertanian', 'uk_tutup']].sum().reset_index()
-        st.dataframe(df_bku_desa, width='stretch', hide_index=True)
+    with kol1c:
+        with st.container(border=True):    
+            st.plotly_chart(grafik_bku_kab, width="content")
+    
+    with kol2c:
+        with st.container(border=True):
+            st.plotly_chart(grafik_target_usaha, width="stretch")
+    
+    with kol3c:
+        with st.container(border=True):
+            st.plotly_chart(grafik_uk_kab, width="content")
+
     with st.expander("REKAP KECAMATAN"):
         df_bku_kec = df_bku.groupby(by=['kec'])[['target_usaha', 'bku_baru', 'bku_baru_non', 'bku_baru_pertanian', 'bku_ditemukan', 'bku_ganda', 'bku_tdk_ditemukan', 'bku_temu_non', 'bku_temu_pertanian', 'bku_tutup', 'uk_baru', 'uk_baru_non', 'uk_baru_pertanian', 'uk_ditemukan', 'uk_ganda', 'uk_tdk_ditemukan', 'uk_temu_non', 'uk_temu_pertanian', 'uk_tutup']].sum().reset_index()
         st.dataframe(df_bku_kec, width='stretch', hide_index=True)
         
         grafik_bku_kec = px.bar(df_bku_kec, x='kec', y=['target_usaha', 'bku_ditemukan', 'bku_tdk_ditemukan', 'bku_ganda', 'bku_tutup', 'bku_baru'], barmode='group', title="Rekap Pendataan BKU per Kecamatan", labels={'value':'Jumlah', 'variable':'Status'})
+
+        grafik_uk_kec = px.bar(df_bku_kec, x='kec', y=['target_usaha', 'uk_ditemukan', 'uk_tdk_ditemukan', 'uk_ganda', 'uk_tutup', 'uk_tutup', 'uk_baru'], barmode='group', title="Rekap Pendataan Usaha Keluarga per Kecamatan", labels={'value':'Jumlah', 'variable':'Status'})
         
         with st.container(border=True):
-            st.plotly_chart(grafik_bku_kec, width="content")
+            st.plotly_chart(grafik_bku_kec, width="stretch")
+
+        with st.container(border=True):
+            st.plotly_chart(grafik_uk_kec, width="stretch")
     
+    with st.expander("REKAP DESA"):
+        df_bku_desa = df_bku.groupby(by=['kec', 'desa'])[['target_usaha', 'bku_baru', 'bku_baru_non', 'bku_baru_pertanian', 'bku_ditemukan', 'bku_ganda', 'bku_tdk_ditemukan', 'bku_temu_non', 'bku_temu_pertanian', 'bku_tutup', 'uk_baru', 'uk_baru_non', 'uk_baru_pertanian', 'uk_ditemukan', 'uk_ganda', 'uk_tdk_ditemukan', 'uk_temu_non', 'uk_temu_pertanian', 'uk_tutup']].sum().reset_index()
+        st.dataframe(df_bku_desa, width='stretch', hide_index=True)
+    
+    with st.expander("PER SLS"):
+        st.dataframe(df_bku, width='stretch', hide_index=True)
+    
+    with st.expander("REKAP PPL"):
+        st.dataframe(usaha_ppl, width='stretch', hide_index=True)
 
 with tab_qc:
     st.subheader("Pendataan Keluarga")
     
-    with st.expander("KUALITAS DATA KELUARGA"):
+    with st.expander("HASIL PENDATAAN KELUARGA"):
        st.dataframe(df_kk, width='stretch', hide_index=True)
 
-    with st.expander("KUALITAS DATA ANGGOTA KELUARGA"):
+    with st.expander("HASIL PENDATAAN ANGGOTA KELUARGA"):
        st.dataframe(df_art, width='stretch', hide_index=True)
 
 # ============================================================
